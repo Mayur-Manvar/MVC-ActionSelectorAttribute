d1889349317985197ddb54b52b7cab9ddf92a93c
Mayur-Manvar <mayurpmanvar@gmail.com>
2020-03-22 20:07:32 +0530

    Adding all files in project.

---
+++ ActionSelector/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionSelector.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [ActionName("Home")]
+        [HttpGet]
+        public string Index()
+        {
+            return "Index Action Method is called.";
+        }
+
+        [ActionName("List")]
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ViewResult Details()
+        {
+            return View("Details");
+        }
+    }
+}
---
+++ ActionSelector/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ActionSelector.MvcApplication" Language="C#" %>
